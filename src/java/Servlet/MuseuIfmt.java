6f536d2d7676b1f51278b42c747a9e5375001b8b
Vanessa <vanessanascimentocy@gmail.com>
2020-02-28 21:43:34 -0300

    Adicionando

---
+++ src/java/Servlet/MuseuIfmt.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author STI
+ */
+@WebServlet(name = "MuseuIfmt", urlPatterns = {"/MuseuIfmt"})
+public class MuseuIfmt extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String titulo = request.getParameter("titulo");
+            String autor = request.getParameter("autor");
+            String tipo = request.getParameter("tipo");
+            String dimensoes = request.getParameter("dimensoes");
+            String ano = request.getParameter("ano");
+            String descricao = request.getParameter("descricao");
+            
+            out.println("<h1>Título : " + titulo + "</h1>");
+            out.println("<h1>Autor : " + autor + "</h1>");
+            out.println("<h1>Tipo : " + tipo + "</h1>");
+            out.println("<h1>Dimensões : " + dimensoes + "</h1>");
+            out.println("<h1>Ano : " + ano + "</h1>");
+            out.println("<h1>Descrição : " + descricao + "</h1>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
